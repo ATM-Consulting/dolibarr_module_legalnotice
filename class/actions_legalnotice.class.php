ea0024195605033a8f51d645b0e95fe00a0905cb
Irvine Fleith <irvine.fleith@atm-consulting.fr>
2024-11-07 14:04:18 +0100

    FIX : Undefined property: stdClass::$INVOICE_FREE_TEXT

--- class/actions_legalnotice.class.php
+++ class/actions_legalnotice.class.php
@@ -133,7 +133,7 @@ class ActionsLegalNotice extends legalnotice\RetroCompatCommonHookActions
                     $legalNotice->product_type != 3) continue;
 
 				if(getDolGlobalString('INVOICE_FREE_TEXT')) $conf->global->INVOICE_FREE_TEXT .= "\n<br />";
-				$conf->global->INVOICE_FREE_TEXT .= $legalNotice->mention;
+				if(getDolGlobalString('INVOICE_FREE_TEXT')) $conf->global->INVOICE_FREE_TEXT .= $legalNotice->mention;
 
 				if(getDolGlobalInt('LEGALNOTICE_DO_NOT_CONCAT')) {
 					break; // On s'arrête à la première mention légale qui réunit toutes les conditions
